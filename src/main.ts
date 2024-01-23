5a7ad4671666d44ad5b05113d418ade01953fc7e
ddong180302 <trandangdong18032002@gmail.com>
2024-01-23 09:57:09 +0700

    commit update file main set global validation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -10,6 +12,9 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+
+  app.useGlobalPipes(new ValidationPipe());
   const port = configService.get<string>('PORT') || 6969;
 
 
