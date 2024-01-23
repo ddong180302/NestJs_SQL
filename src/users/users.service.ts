3a67a3e8ad5d6056d84a92882ebe842759d7d088
ddong180302 <trandangdong18032002@gmail.com>
2024-01-23 18:27:03 +0700

    commit CRUD user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Repository, FindOptionsWhere } from 'typeorm';
 import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
@@ -19,26 +19,54 @@ export class UsersService {
     const hash = hashSync(password, salt);
     return hash;
   }
-  // create(createUserDto: CreateUserDto) {
-  //   return 'This action adds a new user';
-  // }
 
-  async create(email: string, password: string, name: string) {
+  async create(createUserDto: CreateUserDto) {
+    const { email, password, name, phone, age, address } = createUserDto;
     const hashPassword = this.getHashPassword(password);
-    //let user = this.usersRepository.create({ email: email, password: password, name: name })
-    let user = await this.usersRepository.save({ email: email, password: hashPassword, name: name });
+    let user = await this.usersRepository.save({
+      email, password: hashPassword, name, phone, age, address
+    })
     return user;
   }
-  findAll() {
-    return `This action returns all users`;
+
+  async findAll() {
+    return await this.usersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    if (isNaN(id)) {
+      // Nếu id không phải là một số
+      throw new BadRequestException("Invalid id. Must be a number.");
+    }
+    // const conditions: FindOptionsWhere<User> = { id };
+    // const exists = await this.usersRepository.existsBy(conditions);
+    // if (!exists) {
+    //   throw new BadRequestException("User not found!");
+    // } else {
+    //   const user = await this.usersRepository.findOne({ where: { id: id } });
+    //   return user;
+    // }
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new BadRequestException("User not found!")
+    } else {
+      return user;
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const { email, name, phone, age, address } = updateUserDto;
+    if (isNaN(id)) {
+      // Nếu id không phải là một số
+      throw new BadRequestException("Invalid id. Must be a number.");
+    }
+    const user = await this.usersRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new BadRequestException("User not found!")
+    } else {
+      const updated = await this.usersRepository.update({ id: id }, { email, name, phone, age, address });
+      return updated;
+    }
   }
 
   remove(id: number) {
