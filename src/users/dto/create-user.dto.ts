29e097b7b74a7bb34d565693c2ae42dca7bf5e5d
ddong180302 <trandangdong18032002@gmail.com>
2024-01-23 09:54:35 +0700

    commit class validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto { }
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsEmail({}, { message: "email không đúng định dạng!" })
+    @IsNotEmpty({ message: "email không được để trống!" })
+    email: string;
+
+    @IsNotEmpty({ message: "Mật khẩu không được để trống!" })
+    password: string;
+
+    @IsNotEmpty({ message: "Tên không được để trống!" })
+    name: string;
+
+    @IsNotEmpty({ message: "Số điện thoại không được để trống!" })
+    phone: string;
+
+    @IsNotEmpty({ message: "Tuổi không được để trống!" })
+    age: number;
+
+    @IsNotEmpty({ message: "Địa chỉ không được để trống!" })
+    address: string;
+}
